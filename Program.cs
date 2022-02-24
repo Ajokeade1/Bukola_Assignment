9fc9b269a02230265d45e5f4ac821da70024d29f
HP <HP@DESKTOP-U6OBHVE>
2022-02-24 11:56:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace solution
+{
+    class assignment
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "Poly ilaro ";
+
+            Console.Write("Enter second Integer: ");
+            int new_int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second double: ");
+            double new_double = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second string: ");
+            string new_str = Console.ReadLine();
+
+            Console.WriteLine(i + new_int);
+            Console.WriteLine(d + new_double);
+            Console.WriteLine(s + new_str);
+
+        }
+
+    }
+}
+
+
+
